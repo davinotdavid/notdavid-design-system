d39728f6742183f119b3e23ec4d6eb054539867a
Davi Nakano <davinakanoca@gmail.com>
2023-09-22 11:16:09 -0700

    Add color tokens initial docs

--- packages/docs/.storybook/preview.ts
+++ packages/docs/.storybook/preview.ts
@@ -1,4 +1,5 @@
 import type { Preview } from "@storybook/react";
+import { themes } from "@storybook/theming"
 
 const preview: Preview = {
   parameters: {
@@ -12,6 +13,10 @@ const preview: Preview = {
         date: /Date$/,
       },
     },
+    docs: {
+      theme: themes.dark,
+
+    }
   },
 };
 
--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -1,22 +1,22 @@
-import type { Meta, StoryObj } from "@storybook/react"
-import { Button, ButtonProps } from "@notdavid-ui/react"
+import type { Meta, StoryObj } from '@storybook/react'
+import { Button, ButtonProps } from '@notdavid-ui/react'
 
 export default {
   title: 'Button',
   component: Button,
   args: {
-    children: 'Send'
-  }
+    children: 'Send',
+  },
 } as Meta<ButtonProps>
 
 export const Primary: StoryObj<ButtonProps> = {
   args: {
-    size: 'small'
-  }
+    size: 'small',
+  },
 }
 
 export const Large: StoryObj<ButtonProps> = {
   args: {
-    size: 'large'
-  }
+    size: 'large',
+  },
 }
