f2eefb4209e563ebcb2afc9a879be010b7bd600f
Davi Nakano <davinakanoca@gmail.com>
2023-09-22 10:34:19 -0700

    Docs package cleanup and more Storybook settings

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -3,6 +3,29 @@ import { styled } from './styles'
 export const Button = styled('button', {
   fontFamily: '$default',
   backgroundColor: '$primary500',
-  borderRadius: '$md',
-  padding: '$4',
+  borderRadius: '$sm',
+  border: 0,
+  fontWeight: '700',
+  color: '$white',
+
+  variants: {
+    size: {
+      small: {
+        fontSize: '$sm',
+        padding: '$2 $4',
+      },
+      large: {
+        fontSize: '$md',
+        padding: '$3 $6',
+      },
+    },
+  },
+
+  defaultVariant: {
+    size: 'small',
+  },
 })
+
+export type ButtonProps = {
+  size: 'small' | 'large'
+}
