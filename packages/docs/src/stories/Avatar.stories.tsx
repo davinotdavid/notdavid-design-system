3de5cae6d30b0ac4fc29099745967c05a66a9872
Davi Nakano <davinakanoca@gmail.com>
2023-09-25 10:57:26 -0700

    Extends component functionality with argTypes

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -8,6 +8,13 @@ export default {
     src: 'https://github.com/davinotdavid.png',
     alt: 'Davi Nakano',
   },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
